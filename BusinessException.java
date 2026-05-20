f6846066ae535127f250c2b02e4438e2817d2e97
Isabella Sena <isabellasena22@gmail.com>
2026-05-20 19:57:03 -0300

    Delete BusinessException.java

--- BusinessException.java
+++
@@ -1,9 +0,0 @@
-package br.com.sgc.sgc_backend.exception;
-
-public class BusinessException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public BusinessException(String message) {
-        super(message);
-    }
-}
