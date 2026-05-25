bc14f6c2145699a26cc0644ad83b69e5b870f376
Isabella Sena <isabellasena22@gmail.com>
2026-05-25 11:28:34 -0300

    Add files via upload

---
+++ backend/src/main/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package br.com.sgc.sgc_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.sgc.sgc_backend.domain.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	boolean existsByNome(String nome);
+
+}
