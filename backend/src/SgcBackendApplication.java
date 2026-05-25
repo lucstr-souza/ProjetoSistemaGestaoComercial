2084c7a76e3effea696a875e006aa17da1de4015
Luisa Castro Souza <luisa04castro@gmail.com>
2026-05-25 10:34:49 -0300

    feat: adiciona src

---
+++ backend/src/SgcBackendApplication.java
@@ -0,0 +1,12 @@
+package br.com.sgc.sgc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgcBackendApplication.class, args);
+    }
+}
