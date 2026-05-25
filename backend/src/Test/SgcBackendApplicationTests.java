68df1431dd48e73302f685b392cc91b499300c61
Isabella Sena <isabellasena22@gmail.com>
2026-05-25 11:19:38 -0300

    Add files via upload

---
+++ backend/src/Test/SgcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sgc.sgc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
